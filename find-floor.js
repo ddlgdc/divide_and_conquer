e03d30514138f0bbc05a780b9ae19e0e8136d852
ddlgdc <166570618+ddlgdc@users.noreply.github.com>
2024-10-29 17:49:41 -0700

    fifth commit, findFloor updated

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,29 @@
-function findFloor() {
+function findFloor(array, x) {
+    let left = 0;
+    let right = array.length -1;
+    let floor = -1; 
 
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2);
+
+        if (array[middle] === x) {
+            return array[middle];
+        }
+
+        else if (array[middle] < x) {
+            floor = array[middle];
+            left = middle + 1;
+        }
+
+        else {
+            right = middle - 1;
+        }
+    }
+
+    return floor;
 }
 
+console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 9)); // Output: 8
+console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 20)); // Output: 19
+console.log(findFloor([1, 2, 8, 10, 10, 12, 19], 0)); // Output: -1
 module.exports = findFloor
